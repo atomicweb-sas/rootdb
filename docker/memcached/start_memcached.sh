2c00de3d7588c04eab82699a25f2fba8dcf5c8ea
spo <sebastien.porquet@atomicweb.fr>
2023-07-05 15:12:43 +0200

    Add option max-item-size to memcached startup script.

--- docker/memcached/start_memcached.sh
+++ docker/memcached/start_memcached.sh
@@ -5,6 +5,7 @@
   --listen=${MEMCACHED_HOST:-0.0.0.0} \
   --port=${MEMCACHED_PORT:-11211} \
   --memory-limit=${MEMCACHED_MEMUSAGE:-64} \
+  --max-item-size=${MEMCACHED_MAX_ITEM_SIZE:-250m} \
   --conn-limit=${MEMCACHED_MAXCONN:-1024} \
   --threads=${MEMCACHED_THREADS:-4} \
   --max-reqs-per-event=${MEMCACHED_REQUESTS_PER_EVENT:-20} \
