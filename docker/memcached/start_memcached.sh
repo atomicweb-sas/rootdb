75ce261f02379d465c1b1eda8cb1ae125d75f78a
spo <sebastien.porquet@atomicweb.fr>
2023-07-05 15:32:54 +0200

    Rollback option max-item-size to memcached startup script.

--- docker/memcached/start_memcached.sh
+++ docker/memcached/start_memcached.sh
@@ -4,8 +4,7 @@
   --user=${MEMCACHED_USER:-memcached} \
   --listen=${MEMCACHED_HOST:-0.0.0.0} \
   --port=${MEMCACHED_PORT:-11211} \
-  --memory-limit=${MEMCACHED_MEMUSAGE:-2048} \
-  --max-item-size=${MEMCACHED_MAX_ITEM_SIZE:-250m} \
+  --memory-limit=${MEMCACHED_MEMUSAGE:-64} \
   --conn-limit=${MEMCACHED_MAXCONN:-1024} \
   --threads=${MEMCACHED_THREADS:-4} \
   --max-reqs-per-event=${MEMCACHED_REQUESTS_PER_EVENT:-20} \
